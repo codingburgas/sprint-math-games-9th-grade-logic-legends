76666ca268f8a387b07e7e0111058a57affc69bf
PNRusev24 <PNRusev24@codingburgas.bg>
2025-11-26 18:00:11 +0200

    Add game difficulty level

--- main.cpp
+++ main.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, world!" << endl;
-    return 0;
+     int difficultyLevel = 0;
+    cout << "Choose Difficulty" << endl;
+    cout << "1. Easy" << endl;
+    cout << "2. Normal" << endl;
+    cout << "3. Hard" << endl;
+    while(difficultyLevel != 1 && difficultyLevel != 2 && difficultyLevel != 3) {
+    cin >> difficultyLevel;
+    if(difficultyLevel != 1 && difficultyLevel != 2 && difficultyLevel != 3) {
+        cout << "Invalid choice. Please select 1, 2, or 3." << endl;
+    }
+}
 }
