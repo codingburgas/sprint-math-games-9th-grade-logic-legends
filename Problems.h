baf2c8f7ad16b1eccae9fd7db27b5f0b2f9b18b2
Мартин Желев <MAJelev24@codingburgas.bg>
2025-12-06 21:33:54 +0200

    Create Problems.h

---
+++ Problems.h
@@ -0,0 +1,11 @@
+#ifndef PROBLEMS_H
+#define PROBLEMS_H
+ 
+#include "Types.h"
+ 
+void generateProblem(int difficultyLevel, std::string& questionText, int& correctAnswer, bool& trapHere, bool& givesLife, bool& givesBonusPoints);
+void configForDifficulty(int difficultyLevel, int& gridSize, int& startingLives, int& minPointsPerQuestion, int& maxPointsPerQuestion, bool& hasLifeBonusCells, bool& hasBonusPointCells, bool& hasTrapCells);
+void buildGrid(std::string cellQuestions[][MAX_GRID], int cellAnswers[][MAX_GRID], bool cellIsTrap[][MAX_GRID], bool cellGivesLife[][MAX_GRID], bool cellBonusPoints[][MAX_GRID], int gridSize, int difficultyLevel, bool hasLifeBonusCells, bool hasBonusPointCells, bool hasTrapCells);
+int randomInRange(int low, int high);
+ 
+#endif // PROBLEMS_H
