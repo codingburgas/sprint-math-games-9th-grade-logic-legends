2875a06bfb23f1aa711c0f2bfd195d0442cb6c24
DimitarNyagalov <dpnyagalov24@codingburgas.bg>
2025-12-06 20:43:32 +0200

    Create Types.h

---
+++ Types.h
@@ -0,0 +1,15 @@
+#ifndef TYPES_H
+#define TYPES_H
+ 
+#include <string>
+ 
+const int MAX_GRID = 7;
+ 
+// Difficulty levels as simple ints for easy use.
+const int DIFF_EASY = 1;
+const int DIFF_NORMAL = 2;
+const int DIFF_HARD = 3;
+ 
+#endif // TYPES_H
+ 
+ 
