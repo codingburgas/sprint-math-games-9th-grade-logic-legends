d845a36e39f8f21e4d24a13a5c56060cb2c20042
Georgi Nikolov <ginikolov24@codingburgas.bg>
2025-12-07 19:02:32 +0200

    Add makeNormalProblem function for math problems

--- Problems.cpp
+++ Problems.cpp
@@ -25,4 +25,48 @@ namespace
             correctAnswer = a - b;
         }
     }
+
+    // Normal: plus, minus, times, divide.
+    void makeNormalProblem(string& questionText, int& correctAnswer)
+    {
+        int choice = rand() % 4;
+ 
+        switch (choice)
+        {
+            case 0: // addition
+            {
+                int a = randomInRange(1, 30);
+                int b = randomInRange(1, 30);
+                questionText = to_string(a) + " + " + to_string(b) + " = ?";
+                correctAnswer = a + b;
+                break;
+            }
+            case 1: // subtraction
+            {
+                int a = randomInRange(1, 30);
+                int b = randomInRange(1, 30);
+                questionText = to_string(a) + " - " + to_string(b) + " = ?";
+                correctAnswer = a - b;
+                break;
+            }
+            case 2: // multiplication up to 10x10
+            {
+                int a = randomInRange(1, 10);
+                int b = randomInRange(1, 10);
+                questionText = to_string(a) + " x " + to_string(b) + " = ?";
+                correctAnswer = a * b;
+                break;
+            }
+            case 3: // integer division
+            default:
+            {
+                int divisor = randomInRange(1, 10);
+                int multiplier = randomInRange(1, 10);
+                int dividend = divisor * multiplier;
+                questionText = to_string(dividend) + " / " + to_string(divisor) + " = ?";
+                correctAnswer = dividend / divisor;
+                break;
+            }
+        }
+    }
 }
